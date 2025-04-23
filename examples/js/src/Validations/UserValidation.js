601df416902c7266f50212fc47494a37e4259f14
analuisamessias <analuisa.mendes1712@gmail.com>
2025-04-23 16:26:39 -0300

    feat: implement pr-review and test it

--- examples/js/src/Validations/UserValidation.js
+++ examples/js/src/Validations/UserValidation.js
@@ -1,16 +1,18 @@
-const ValidatorUtil = require("../Utils/ValidatorUtil")
+const ValidatorUtil = require("../Utils/ValidatorUtil");
 
 class UserValidation {
-	validateCreateUserParams (user) {
-		return ValidatorUtil.validate(user, {
-			name: [
-				{
-					handler: value => Boolean(value),
-					errorMessage: "must_be_filled"
-				}
-			]
-		})
-	}
+  validateCreateUserParams(user) {
+    return ValidatorUtil.validate(user, {
+      name: [
+        {
+          handler: (value) => Boolean(value),
+          errorMessage: "must_be_filled",
+        },
+      ],
+    });
+  }
 }
 
-module.exports = new UserValidation()
+module.exports = new UserValidation();
+
+console.log("UserValidation loaded");
